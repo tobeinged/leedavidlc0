089110e466b4c19b93de90a61520647403872c96
lee david <keersun@qq.com>
2018-10-17 09:12:09 +0800

    cuda 9.2

---
+++ ggzero_bash2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#wget http://developer.download.nvidia.com/compute/redist/cudnn/v7.1.4/cudnn-9.0-linux-x64-v7.1.tgz
+#tar -xzvf cudnn-9.0-linux-x64-v7.1.tgz
+#cp -P cuda/include/cudnn.h /usr/local/cuda/include/cudnn.h
+#cp -P cuda/lib64/libcudnn* /usr/local/cuda/lib64/
+#chmod a+r /usr/local/cuda/lib64/libcudnn*
+wget -c https://github.com/leedavid/leela-chess-to-Chinese-Chess/raw/master/lc0/ggzero_linux
+wget -c https://github.com/leedavid/leela-chess-to-Chinese-Chess/raw/master/lc0/lc0
+chmod 777 lc0
+chmod 777 ggzero_linux
