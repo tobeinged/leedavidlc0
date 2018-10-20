bc1683fa98099b34821990c7e1eb10a496c58808
lee david <keersun@qq.com>
2018-10-20 16:00:38 +0800

    colab cuda9.2 catlog ok

--- lc0/ggzero_bash.sh
+++ lc0/ggzero_bash.sh
@@ -6,6 +6,8 @@ apt-get install protobuf-compiler
 #cp -P cuda/lib64/libcudnn* /usr/local/cuda/lib64/
 #chmod a+r /usr/local/cuda/lib64/libcudnn*
 
+
+
 apt-get update
 apt-get install -y --fix-missing --no-install-recommends cuda-compiler-9-2 cuda-cublas-dev-9-2 cuda-cudart-dev-9-2
 apt-get install -y --fix-missing --no-install-recommends nvidia-opencl-dev libopenblas-dev
@@ -14,7 +16,9 @@ wget http://developer.download.nvidia.com/compute/redist/cudnn/v7.3.1/cudnn-9.2-
 cd /usr/local && tar -xzvf /content/cudnn-9.2-linux-x64-v7.3.1.20.tgz
 chmod a+r /usr/local/cuda/lib64/libcudnn*
 
+cd /content
 wget -c https://github.com/leedavid/leela-chess-to-Chinese-Chess/raw/master/lc0/ggzero_linux
 wget -c https://github.com/leedavid/leela-chess-to-Chinese-Chess/raw/master/lc0/lc0
 chmod 777 lc0
 chmod 777 ggzero_linux
+
