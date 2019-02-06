b1553dbd14c7a985b71c7607fa49b9037a93b047
lee david <keersun@qq.com>
2019-02-06 16:48:53 +0800

    Update ggzero_bash.sh

--- lc0/ggzero_bash.sh
+++ lc0/ggzero_bash.sh
@@ -6,13 +6,13 @@ apt-get install protobuf-compiler
 #cp -P cuda/lib64/libcudnn* /usr/local/cuda/lib64/
 #chmod a+r /usr/local/cuda/lib64/libcudnn*
 
-apt-get update
-apt-get install -y --fix-missing --no-install-recommends cuda-compiler-9-2 cuda-cublas-dev-9-2 cuda-cudart-dev-9-2
-apt-get install -y --fix-missing --no-install-recommends nvidia-opencl-dev libopenblas-dev
+#apt-get update
+#apt-get install -y --fix-missing --no-install-recommends cuda-compiler-9-2 cuda-cublas-dev-9-2 cuda-cudart-dev-9-2
+#apt-get install -y --fix-missing --no-install-recommends nvidia-opencl-dev libopenblas-dev
 
-wget http://developer.download.nvidia.com/compute/redist/cudnn/v7.3.1/cudnn-9.2-linux-x64-v7.3.1.20.tgz
-cd /usr/local && tar -xzvf /content/cudnn-9.2-linux-x64-v7.3.1.20.tgz
-chmod a+r /usr/local/cuda/lib64/libcudnn*
+#wget http://developer.download.nvidia.com/compute/redist/cudnn/v7.3.1/cudnn-9.2-linux-x64-v7.3.1.20.tgz
+#cd /usr/local && tar -xzvf /content/cudnn-9.2-linux-x64-v7.3.1.20.tgz
+#chmod a+r /usr/local/cuda/lib64/libcudnn*
 
 cd /content
 wget -c https://github.com/leedavid/leela-chess-to-Chinese-Chess/raw/master/lc0/ggzero_linux
