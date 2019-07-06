c9e8daf6fd442f0e7ef316a0f633b1d938e61630
lee david <keersun@qq.com>
2019-07-06 18:55:37 +0800

    del old lc0

--- lc0/ggzero_bash.sh
+++ lc0/ggzero_bash.sh
@@ -7,14 +7,17 @@ apt-get install protobuf-compiler
 #chmod a+r /usr/local/cuda/lib64/libcudnn*
 
 apt-get update
-apt-get install -y --fix-missing --no-install-recommends cuda-compiler-10-0 cuda-cublas-dev-10-0 cuda-cudart-dev-10-0
+#apt-get install -y --fix-missing --no-install-recommends cuda-compiler-10-0 cuda-cublas-dev-10-0 cuda-cudart-dev-10-0
 apt-get install -y --fix-missing --no-install-recommends nvidia-opencl-dev libopenblas-dev
 
-wget http://developer.download.nvidia.com/compute/redist/cudnn/v7.3.1/cudnn-10.0-linux-x64-v7.3.1.20.tgz
-cd /usr/local && tar -xzvf /content/cudnn-10.0-linux-x64-v7.3.1.20.tgz
-chmod a+r /usr/local/cuda/lib64/libcudnn*
+#wget http://developer.download.nvidia.com/compute/redist/cudnn/v7.3.1/cudnn-10.0-linux-x64-v7.3.1.20.tgz
+#cd /usr/local && tar -xzvf /content/cudnn-10.0-linux-x64-v7.3.1.20.tgz
+#chmod a+r /usr/local/cuda/lib64/libcudnn*
 
 cd /content
+rm -f ggzero_linux
+rm -f lc0
+
 wget -c https://github.com/leedavid/leela-chess-to-Chinese-Chess/raw/master/lc0/ggzero_linux
 wget -c https://github.com/leedavid/leela-chess-to-Chinese-Chess/raw/master/lc0/lc0
 chmod 777 lc0
